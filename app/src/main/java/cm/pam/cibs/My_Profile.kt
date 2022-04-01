dbf4af20a1eae033e252f0f4c42351d24ec2f0a1
ISRAEL_TIENTCHEU <tientcheuisrael@gmail.com>
2022-04-01 02:31:36 +0100

    Add My_profile design activity with picture, username and other for this person who are connected in the app

---
+++ app/src/main/java/cm/pam/cibs/My_Profile.kt
@@ -0,0 +1,11 @@
+package cm.pam.cibs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class My_Profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_profile)
+    }
+}
